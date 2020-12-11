966649af16c6d9178a476e5559b99d255a64270e
zkristof <zkristof@gmail.com>
2020-12-11 17:10:20 +0100

    PUT,PATCH, DELETE

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from profiles_api import views
 
+
 urlpatterns = [
-    path('hello-views/', views.HelloApiView.as_views()),
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
